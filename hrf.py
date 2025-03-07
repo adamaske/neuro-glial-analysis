2cf5117f250d01caaf930c6bd474cd2489eca8b2
Adam Aske <adamaske@live.no>
2025-03-07 14:59:32 +0100

    hrf and betas

--- hrf.py
+++ hrf.py
@@ -1,47 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.special as spc
 
-# Parameters
+def gamma(x, alpha, beta):
+    return (beta**alpha / spc.gamma(alpha)) * x**(alpha) * np.exp(-beta * x)
+
+def chrf(t, a1, a2, b1, b2, c):
+    """ Computes a double gamma canonical hemodynamic response function."""
+    hrf = gamma(t, a1, b2) - (c* gamma(t, a2,b2))
+    return hrf
+
+def test_thetas():
+    thetas = [[6, 16, 1, 1, 1/6],
+              [5, 17, 2, 1, 1/10],
+              [4, 18, 1, 3, 1/12],
+              ]
+
+    colors = [ "red", "green", "blue", "orange"]
+    duration = 30
+    sampling_frequency = 5.1
+    time = np.linspace(0, duration, int(duration*sampling_frequency))
+    for i, t in enumerate(thetas):
+        a1 = t[0]
+        a2 = t[1]
+        b1 = t[2]
+        b2 = t[3]
+        c = t[4]
+
+        hrf = chrf(time, a1, a2, b1, b2, c)
+        print(f"cHRF : a1={a1}, a2={a2}, b1={b1}, b2={b2}, c={c}")
+
+        label = f"a1={a1}, a2={a2}, b1={b1}, b2={b2}, c={c}" #create label
+        plt.plot(time, hrf, color=colors[i], label=label)
+
+
+    plt.title(f'Double Gamma HRF', fontsize=20)
+    plt.xlabel('Time (s)', fontsize=20)
+    plt.ylabel('Amplitude', fontsize=20)
+    plt.legend(fontsize=20) # Adjust fontsize here
+    plt.grid(True)
+    plt.show()
+
+#Parameters
 duration = 15  # seconds
 sampling_rate = 5  # Hz
 num_samples = int(duration * sampling_rate)
-mean = duration / 2  # Center the Gaussian at the middle of the duration
-std_dev = 2  # Adjust the width of the Gaussian
-
-# Time vector
 time = np.linspace(0, duration, num_samples)
-
-# Normal distribution time series
-#mean = 0
-#std_dev = 1
-#normal_distribution_time_series = np.random.normal(mean, std_dev, num_samples)
-
-
-def double_gamma(time, peak1, peak2, width1, width2):
-    """Generates a double gamma function."""
-    gamma1 = (time / peak1)**2 * np.exp(-(time - peak1) / width1)
-    gamma2 = (time / peak2)**2 * np.exp(-(time - peak2) / width2)
-    return gamma1 - gamma2
-
-# Generate double gamma function
-g_t = np.linspace(0, 30, int(30*sampling_rate))
-# Generate double gamma function
-peak1 = 3
-peak2 = 6
-width1 = 1
-width2 = 2
-double_gamma_function = double_gamma(g_t, peak1, peak2, width1, width2)
-
-plt.plot(g_t, double_gamma_function, label="Double Gamma Function")
-plt.title("Double Gamma Function")
-plt.xlabel("Time (seconds)")
-plt.ylabel("Amplitude")
-plt.legend()
-plt.grid(True)
-
-plt.tight_layout()
-plt.show()
-
+mean = 0
+std_dev = 1
 # Gaussian time series
 gaussian_time_series = np.exp(-0.5 * ((time - mean) / std_dev)**2)
 
